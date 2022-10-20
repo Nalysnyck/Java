2bca85cb709b0532d9c43443584f5ef0213a07ab
Shylo_Bohdan <108338577+Nalysnyck@users.noreply.github.com>
2022-10-20 15:16:56 +0300

    Delete Laboratorna 6 directory

--- Laboratorna 6/com/mobileconnection/Main.java
+++
@@ -1,9 +0,0 @@
-package mobileconnection;
-
-import static mobileconnection.menu.MainMenu.callMainMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        callMainMenu();
-    }
-}
--- Laboratorna 6/com/mobileconnection/command/Command.java
+++
@@ -1,5 +0,0 @@
-package mobileconnection.command;
-
-public interface Command {
-    void execute();
-}
